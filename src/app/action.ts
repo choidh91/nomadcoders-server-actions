c2b0e7c4aa3528dc99d96fb26ba3980c628f35ff
choidh <choidh@hancom.com>
2024-05-24 10:29:28 +0900

    add zod validation

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,17 +1,45 @@
 "use server";
+import { z } from "zod";
+
+const numberRegex = new RegExp("[0-9]");
+
+const loginSchema = z.object({
+  name: z.string().min(5, "Username should be at least 5 characters long."),
+  email: z
+    .string()
+    .email()
+    .refine(
+      (email) => email.endsWith("@zod.com"),
+      "Only @zod.com emails are allowed."
+    ),
+  password: z
+    .string()
+    .min(10, "Password should be at least 10 characters long.")
+    .regex(
+      numberRegex,
+      "Password should contain at least one number (0123456789)."
+    ),
+});
 
 export async function login(prevState: any, formData: FormData) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  const password = formData.get("password");
 
-  if (password === "12345") {
-    return {
-      status: "success",
-    };
-  } else {
+  const data = {
+    name: formData.get("name"),
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+
+  const result = loginSchema.safeParse(data);
+
+  if (!result.success) {
     return {
       status: "error",
-      errors: ["Wrong password"],
+      error: result.error.flatten(),
     };
   }
+
+  return {
+    status: "success",
+  };
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import { useFormState } from "react-dom";
 
 const initialState = {
   status: "",
-  errors: [],
+  error: undefined,
 };
 
 export default function Home() {
@@ -23,31 +23,43 @@ export default function Home() {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             className="w-10 h-10"
           >
             <path
               stroke="#f87171"
               fill="#f87171"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z"
             />
             <path
               fill="white"
               stroke="white"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeLinecap="round"
+              strokeLinejoin="round"
               d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z"
             />
           </svg>
         </div>
         <form action={formAction} className="flex flex-col gap-3">
-          <Input name="email" placeholder="Email" required type="email">
+          <Input
+            name="email"
+            placeholder="Email"
+            required
+            type="email"
+            errors={state.error?.fieldErrors.email}
+          >
             <EnvelopeIcon className="size-6" />
           </Input>
-          <Input name="name" placeholder="Username" required type="text">
+          <Input
+            name="name"
+            placeholder="Username"
+            required
+            type="text"
+            errors={state.error?.fieldErrors.name}
+          >
             <UserIcon className="size-6" />
           </Input>
           <Input
@@ -55,7 +67,7 @@ export default function Home() {
             placeholder="Password"
             required
             type="password"
-            errors={state.errors}
+            errors={state.error?.fieldErrors.password}
           >
             <KeyIcon className="size-6" />
           </Input>
